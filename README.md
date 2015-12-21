27d112d39679f8b02a706b53a06501bf2ac2a940
Niklas Köhler <kontakt@niklaskoehler.de>
2015-12-21 15:10:15 +0100

    Convert simple function to jQuery plugin

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # same-height
-This short jQuery function set all elements in a list to the biggest calculated height
+This short jQuery plugin sets all elements in a list to the biggest calculated height
 
 ## Usage
-~~~
-sameHeight('.container', '.teaser');
-~~~
+```javascript
+$('.target').sameHeight('.children');
+```
 This sets all .teaser to same height in container.
+
+## Events
+```javascript
+$('.target').trigger('EVENT');
+```
+
+After the plugin has been bound to the element it delivers some events:
+
+| Event | Description |
+| ------------- |:-------------:|
+| updateSize| recalculates the height of each children (like initial call) |
+| destroySize | remove the height of each children |
--- src/jquery.same-height.js
+++ src/jquery.same-height.js
@@ -1,20 +1,64 @@
+'use strict';
 /*global $, jQuery */
 
 /**
- * set elements target in list to same height
+ * Set elements target in list to same height
  */
+ $.fn.sameHeight = function(children) {
+    var self = this;
 
-function sameHeight(list, target) {
-    'use strict';
+    /**
+     * Container element that contains children
+     */
+    this.container = this;
 
-    var elements  = list.find(target),
+    /**
+     * Children elements. If not given we just select all first level childs
+     */
+    this.children = (children) ? this.container.find(children) : this.container.find('> *');
+
+    /**
+     * Event to update the height of each children
+     */
+    this.container.bind('updateSize',function() {
+        self._setHeight();
+    });
+
+    /**
+     * Event to reset the height of each children
+     */
+    this.container.bind('destroySize',function() {
+        self._removeHeight();
+    });
+
+    /**
+     * Function to set the height of each children
+     */
+    this._setHeight = function() {
+        var children  = this.children,
         heights   = [],
         maxHeight = null;
 
-    elements.each(function () {
-        $(this).removeAttr('style');
-        heights.push($(this).height());
-    });
-    maxHeight = Math.max.apply(null, heights);
-    elements.height(maxHeight);
-}
+        children.each(function () {
+            $(this).height('');
+            heights.push($(this).innerHeight());
+        });
+
+        maxHeight = Math.max.apply(null, heights);
+        children.height(maxHeight);
+    };
+
+    /**
+     * Function to remove the height of each children
+     */
+    this._removeHeight = function() {
+         this.children.height('');
+    };
+
+    /**
+     * Trigger setHeight-function to set the height of each childrian initial
+     */
+    this._setHeight();
+
+    return this;
+};
